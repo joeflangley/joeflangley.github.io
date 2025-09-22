ffc4651de7f0066b313084c2e7ddee9013a5597c
Joe Langley <jfl49@cam.ac.uk>
2025-09-22 09:20:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,46 +12,7 @@ function toggleDropdown(buttonId, dropdownId) {
 toggleDropdown("projects-button", "projects-dropdown");
 
 // Sub-dropdowns
-toggleDropdown("species-button", "species-dropdown");
-toggleDropdown("areas-button", "areas-dropdown");
-
-
-/* // Get the dropdown button and the div that contains project links
-const projectButton = document.getElementById("research-drop-one-button");
-const projectDropdown = document.getElementById("research-drop-one");
-
-// Hide the dropdown by default
-projectDropdown.style.display = "none";
-
-// Toggle the dropdown when the button is clicked
-projectButton.addEventListener("click", () => {
-    if (projectDropdown.style.display === "none") {
-        projectDropdown.style.display = "block"; // Show the dropdown
-    } else {
-        projectDropdown.style.display = "none";  // Hide the dropdown
-    }
-}); */
-
-/* // List of image file paths
-const images = [
-    "./photos/elephants1.png",
-    "./photos/lion_cub.png"
-];
-
-// Get the <img> element
-const slideshow = document.getElementById("slideshow");
-
-// Index to track the current image
-let currentIndex = 0;
-
-// Function to change the image
-function changeImage() {
-    currentIndex = (currentIndex + 1) % images.length; // loop back to start
-    slideshow.src = images[currentIndex];
-}
-
-// Change image every 5 seconds (5000ms)
-setInterval(changeImage, 3000); */
+toggleDropdown("places-button", "places-dropdown");
 
 const images = [
     "./photos/elephants1.png",
@@ -87,11 +48,11 @@ function updateDots() {
     });
 }
 
-// Auto-change every 5s
+// Auto-change every 7.5s
 setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
-}, 5000);
+}, 7500);
 
 // Show first image + highlight first dot
 showImage(currentIndex);
